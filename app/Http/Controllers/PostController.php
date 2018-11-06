d2532b0f2d90b5e7a2a26dfc71c6dd263da69d45
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-11-06 13:55:55 +0300

    add Post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function create(Request $request)
+    public function create(Request $request, Post $post)
     {
-        return 'post will be created';
+        $createdPost = $request->user()->posts()->create([
+            'body' => $request->body
+        ]);
+
+        return response()->json($post->with('user')->find($createdPost->id));
     }
 }
