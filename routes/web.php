6163a5bd5517f7549f4b7540e292931ad6eff39d
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-11-06 19:20:58 +0300

    add json response

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ Auth::routes();
 
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/', 'TimelineController@index');
+    Route::get('/posts', 'PostController@index');
     Route::post('/posts', 'PostController@create');
 
     Route::get('/users/{user}', 'UserController@index')->name('users');
