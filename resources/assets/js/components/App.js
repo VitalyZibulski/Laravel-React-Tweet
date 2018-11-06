b1d8162b269e4f63e4e1ecf9606e73acd30f9728
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-11-06 12:13:15 +0300

    fix React component

---
+++ resources/assets/js/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8 col-md-offset-2">
+                        <div className="panel panel-default">
+                            <div className="panel-heading">App Component</div>
+
+
+                        <div className="panel-body">
+                            I am an app component!
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        );
+    }
+}
+
+export default App;
